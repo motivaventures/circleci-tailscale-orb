d8fc675c6ae43d11fab7c77d5fed438f3f2f16b2
Tega McKinney <tega.mckinney@motivaventures.com>
2022-04-18 13:12:39 -0400

    clean-up

--- src/scripts/start-tailscaled.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-sudo tailscaled --tun=userspace-networking --outbound-http-proxy-listen=localhost:"${HTTP_PROXY_PORT}" --socks5-server=localhost:"${SOCKS5_PROXY_PORT}" 2>~/tailscaled.log &
